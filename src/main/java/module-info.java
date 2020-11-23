7c72417f8f74cf2acaf3fde5138e6515e72d0eb2
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-11-23 22:44:51 +0100

    added httpclient and gson libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module weather.app {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.net.http;
+    requires com.google.gson;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.apache.httpcomponents.httpclient;
 
     opens application;
     opens application.controller;
